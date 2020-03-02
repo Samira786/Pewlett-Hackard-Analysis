9a6c50efbe890d64c40ddfe1ab8f2f82e255fa4a
Samira786 <59812357+Samira786@users.noreply.github.com>
2020-03-01 21:59:32 -0800

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -110,3 +110,159 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 --View the Table
 SELECT * FROM retirement_info;
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+--joining departments and dept_manager tables
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+--joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date,
+	de.dept_no
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+--join departments and dept manager and make new table
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+INTO dept_dept_mgr
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+
+		
+--Department retirees
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info_sales_dev
+FROM current_emp AS ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+on (de.dept_no = d.dept_no)
+WHERE de.to_date = ('9999-01-01') AND d.dept_name IN ('Sales','Development');
+
+SELECT * FROM dept_info_sales_dev
+
+--Challenge
+
+-- Create new table for number of titles retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement 
+FROM employees
+
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Check the table
+SELECT * FROM retirement
+
+--Join Title, from_date and Salary to the query 
+SELECT e.emp_no, e.first_name, e.last_name,
+	t. title, t.from_date,
+	s. salary
+INTO retirement_title
+From employees AS e
+INNER JOIN titles AS t
+ON (e. emp_no = t. emp_no)
+INNER JOIN salaries AS s
+ON (e.emp_no = s. emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+ORDER BY from_date desc;
+
+SELECT * FROM retirement_title;
+
+
+SELECT COUNT (*) FROM retirement_title
+
+
+SELECT * FROM retirement_title
+
+--Counting the number of each employee per title
+SELECT COUNT (title), title
+INTO unique_title 
+FROM retirement_title
+Group BY title
+ORDER BY count DESC;
+SELECT * FROM unique_title
+
+--Employees most recent title
+SELECT emp_no, first_name, last_name,  MAX(from_date) maxDate
+	   FROM retirement_title
+	   GROUP BY emp_no, first_name, last_name;
+
+
+--Who's ready for a Mentor
+SELECT e. emp_no, e. first_name, e. last_name, t. title, t. from_date, t. to_date
+INTO Mentors 
+FROM employees As e
+INNER JOIN  titles AS t
+ON (e.emp_no = t. emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (to_date = '9999-01-01');
+
+SELECT * From Mentors
+
